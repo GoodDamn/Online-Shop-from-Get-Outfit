38f49457044b5741839022380c7990050c6dc1b8
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2021-03-21 23:41:10 +0300

    Load data from server.getoutfit.ru and design app.

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,31 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' show jsonDecode, utf8;
 
-String idCategoryProduct;
-
 bool visibleProgress = false;
 
-List dataProducts, dataCategory;
-String limit = "12";
-String name;
+List dataProducts, dataCategory, details;
+int detailIndex;
+String limit = "12", name;
 const String offersUrl = "http://server.getoutfit.ru/offers",
   categoriesUrl = "http://server.getoutfit.ru/categories";
+
+Future<String> getProductDetails(String url) async {
+  var response = await http.get(
+    Uri.encodeFull(url),
+  );
+  print(response);
+
+  visibleProgress = true;
+  var convertDataToJson = utf8.decode(response.bodyBytes);
+  convertDataToJson = "{\"results\": " + convertDataToJson + "}";
+  print(convertDataToJson);
+  var decodeJson = jsonDecode(convertDataToJson);
+  details = decodeJson['results'];
+  visibleProgress = false;
+
+  return "Success";
+}
+
 
 Future<String> getJsonDataForCategories(String url) async {
   var response = await http.get(
@@ -154,7 +170,8 @@ class Search extends StatelessWidget {
                               onTap: () {
                                 Navigator.push(context, MaterialPageRoute(
                                     builder: (context) => ProductDetail()));
-                                idCategoryProduct = dataProducts[index]['categoryId'].toString();
+                                getProductDetails(offersUrl + "?categoryId=" + dataProducts[index]['categoryId'].toString());
+                                detailIndex = index;
                               },
                               child: Card(
                                 child: Container(
@@ -239,7 +256,10 @@ class ProductDetail extends StatelessWidget{
       ),
       body: Center(
         child: Column(
-
+          children: <Widget>[
+            Text(details[detailIndex]['name']),
+            Text(details[detailIndex]['currencyId'])
+          ],
         ),
       ),
     );
